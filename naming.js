3d9d144120311f274c18057c67817deaa52beeb8
aysha3f <tahiaysha220@gmail.com>
2025-03-09 01:36:28 +0600

    first commit

---
+++ naming.js
@@ -0,0 +1,17 @@
+// 1.no keywords in variable name
+var false = 45;
+var var = 13;
+// var,false is keyword that's why you cannot use them in variable name.
+
+
+// 2. no space or gap in variable name
+var is happy = false;
+var ishappy = true;
+// first variable is not correct bcoz you can't use gap is variable name
+
+
+// 3.no quote
+var "address" = "khocu kete";
+// this one is wrong because you can't use  quotetion ("") in variable name
+
+
---
+++ types.js
@@ -0,0 +1,14 @@
+// string
+var name = "komal";
+console.log("komal")
+var address = ("Bandorbon");
+// boolean
+var isRich = true;
+console.log(isRich)
+var passed = false;
+console.log(passed)
+console.log( typeof passed)
+// number
+var price = 500;
+console.log(price)
+console.log( typeof price)
